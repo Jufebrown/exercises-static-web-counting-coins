8e6b9e60b9228e82a61571462ca580227222ef49
Jufebrown <jufebrown@gmail.com>
2016-11-22 11:05:08 -0600

    added amountAndRemainder function and moved object into coinCounter function

--- coins.js
+++ coins.js
@@ -13,25 +13,26 @@
 var inputAmount = prompt("Enter an amount of money ex: 4.77");
 var cents=Math.round(inputAmount*100)%100;
 
-// Initialize a JavaScript object to hold the coins
-var coinPurse = {dollars:0,quarters:0,dimes:0,nickels:0,pennies:0}
-//break dollar amount off
-coinPurse.dollars = Math.floor(inputAmount);
-  
 
+function amountAndRemainder(coinValue) {
+  var currentCoin = Math.floor(cents/coinValue);
+  cents = cents % coinValue;
+  return currentCoin;
+}
 
 function coinCounter () {
-//take cents and divide by 25, get remainder
-  coinPurse.quarters = Math.floor(cents/25);
-  cents = cents % 25;
-//take remainder and divide by 10, get remainder
-  coinPurse.dimes = Math.floor(cents/10);
-  cents = cents % 10;
-//take remainder and divide by 5, get remainder
-  coinPurse.nickels = Math.floor(cents/05);
-  cents = cents % 05;
-//set remainder to pennies
-  coinPurse.pennies = Math.floor(cents/01);
+  // Initialize a JavaScript object to hold the coins
+  var coinPurse = {dollars:0,quarters:0,dimes:0,nickels:0,pennies:0}
+  //break dollar amount off
+  coinPurse.dollars = Math.floor(inputAmount);
+  //take cents and divide by 25, get remainder
+  coinPurse.quarters = amountAndRemainder(25);
+  //take remainder and divide by 10, get remainder
+  coinPurse.dimes = amountAndRemainder(10);
+  //take remainder and divide by 5, get remainder
+  coinPurse.nickels = amountAndRemainder(5);
+  //set remainder to pennies
+  coinPurse.pennies = amountAndRemainder(1);
   return coinPurse;
 }
 
